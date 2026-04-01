23ee8909b0d1a89e8904e9d08343f906a8fb5324
Christoph Jerolimov <jerolimov+git@gmail.com>
2026-04-01 10:57:59 +0200

    cleanup

--- scripts/create-pipelines.sh
+++ scripts/create-pipelines.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+dirname=$(dirname $0)
+
 namespace="christoph-backstage-test"
 
 oc new-project "$namespace"
 
 for i in $(seq 1 7); do
-    oc create -n "$namespace" -f ../features/tekton/christoph-demo1-service-pipeline-run.yaml
+    oc create -n "$namespace" -f "$dirname/../features/tekton/christoph-demo1-service-pipeline-run.yaml"
 done
 
 for i in $(seq 1 7); do
-    oc create -n "$namespace" -f ../features/tekton/christoph-demo2-service-pipeline-run.yaml
+    oc create -n "$namespace" -f "$dirname/../features/tekton/christoph-demo2-service-pipeline-run.yaml"
 done
 
 for i in $(seq 1 7); do
-    oc create -n "$namespace" -f ../features/tekton/christoph-demo3-service-pipeline-run.yaml
+    oc create -n "$namespace" -f "$dirname/../features/tekton/christoph-demo3-service-pipeline-run.yaml"
 done
