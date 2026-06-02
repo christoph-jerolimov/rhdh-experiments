ac7fc3876d99e39a6abc94493fea71aab76aa53f
Christoph Jerolimov <jerolimov+git@gmail.com>
2026-06-02 10:58:07 +0200

    update tekton test pipeline(run)s

--- scripts/create-pipelines.sh
+++ scripts/create-pipelines.sh
@@ -2,18 +2,18 @@
 
 dirname=$(dirname $0)
 
-namespace="christoph-backstage-test"
+namespace="christoph"
 
 oc new-project "$namespace"
 
-for i in $(seq 1 7); do
+for i in $(seq 1 3); do
     oc create -n "$namespace" -f "$dirname/../features/tekton/christoph-demo1-service-pipeline-run.yaml"
 done
 
-for i in $(seq 1 7); do
+for i in $(seq 1 5); do
     oc create -n "$namespace" -f "$dirname/../features/tekton/christoph-demo2-service-pipeline-run.yaml"
 done
 
-for i in $(seq 1 7); do
+for i in $(seq 1 23); do
     oc create -n "$namespace" -f "$dirname/../features/tekton/christoph-demo3-service-pipeline-run.yaml"
 done
